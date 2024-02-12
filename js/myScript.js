0d8a773e7885cc4ef7117400de27bb41c4fc5f5e
FabianGoji <camarillorichard.1@gmail.com>
2024-02-12 12:44:58 -0500

    fixed link directioning

--- js/myScript.js
+++ js/myScript.js
@@ -1,3 +1,5 @@
+//trying to generate toc content through js
+
 // let topics = document.querySelectorAll('.topic_container');
 // let toc = [];
 
@@ -24,3 +26,25 @@
 //     tocContent.innerHTML = topic_name;
 //     tocContainer.appendChild(tocContent);
 // });
+
+
+//getting the links to a scroll position that accounts for header height
+
+function scrollToHeading(event) {
+    event.preventDefault(); // Prevent the default link behavior
+    var targetElement = document.querySelector(event.target.getAttribute('href'));
+    console.log(targetElement);
+    var headerHeight = document.querySelector('.header').offsetHeight;
+    console.log(headerHeight);
+    if (targetElement){
+      window.scrollTo({
+        top: targetElement.offsetTop - headerHeight
+      });
+    }
+  }
+  
+  // Attaches the event listener to each link in "stactic_col"
+  var staticLinks = document.querySelectorAll('.static_col a');
+  for (var i =  0; i < staticLinks.length; i++) {
+    staticLinks[i].addEventListener('click', scrollToHeading);
+  }
